8ae3cae1079c43c8d159ac37f1a982cce4acc2b7
VIVEK <99870677+Roninopp@users.noreply.github.com>
2025-10-27 16:45:45 +0530

    Add files via upload

---
+++ naruto_bot/handlers/__init__.py
@@ -0,0 +1,15 @@
+# naruto_bot/handlers/__init__.py
+# This file makes 'handlers' a Python package.
+
+# Import all handlers to make them accessible
+from .core_handlers import register_core_handlers
+from .activity_handlers import register_activity_handlers
+from .jutsu_handlers import register_jutsu_handlers
+from .battle_handlers import register_battle_handlers
+
+def register_all_handlers(application):
+    """A helper function to register all handlers with the bot application."""
+    register_core_handlers(application)
+    register_activity_handlers(application)
+    register_jutsu_handlers(application)
+    register_battle_handlers(application)
